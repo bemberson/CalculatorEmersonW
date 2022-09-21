ab64d72ebb85c7a5cae2bbbe7b97514430bf921e
bemberson <emersonwest16@gmail.com>
2022-09-20 22:21:04 -0500

    Calculator Example

---
+++ Operator-Exercise/main.swift
@@ -0,0 +1,21 @@
+var x = 50
+var y = 25
+//=========================SECTION 2 =============
+
+var add = x + y
+var subtract = x - y
+var multiply = x * y
+var divide = x/y
+var percent = (x/y)*100
+
+//=========================SECTION 3====================
+print("The added value is ")
+print(add)
+print("The subtracted value is ")
+print(subtract)
+print("The multiplied value is ")
+print(multiply)
+print("The divided value is ")
+print(divide)
+print("The percantage of the two is ")
+print(percent)
